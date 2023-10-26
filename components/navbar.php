f0f712caa8bcf1c476052f205f165261b7cecc3b
valentputraa <mvalentmp12@gmail.com>
2023-10-26 15:14:58 +0800

    first commit

---
+++ components/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg bg-body-primary d-flex justify-content-around">
+      <a class="navbar-brand" href="#">
+        <img src="../assets/Baby panda.png" alt="logo" height="100">
+      </a>  
+        <ul class="navbar-nav gap-5 fs-3">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Baju</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Celana</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Hoodie</a>
+          </li>
+        </ul>
+      </div>   
+      <div>
+        <form action="search.php" method="GET" class="d-flex">
+          <input type="text" name="query" placeholder="Search" class="form-control"  style="max-width: 150px;">
+          <input type="submit" style="display: none;">
+          <input type="image" src="../assets/search logo.png" width="50px" alt="Cari" class="btn btn-outline-secondary ms-2">
+      </form>
+      </div>
+    </nav>
